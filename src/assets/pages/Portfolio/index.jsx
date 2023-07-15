25c7dba7a042450b1aba0bb655d8a5ee8307acbf
pasaribuputri <pasaribuutri02@gmail.com>
2023-07-15 12:28:09 +0700

    coo

--- src/assets/pages/Portfolio/index.jsx
+++ src/assets/pages/Portfolio/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import proyek from '../../components/proyek'
+import Proyek from '../../components/proyek'
 
 
 const projek = [
@@ -32,7 +32,7 @@ const Portfolio = () => {
         <div className='w-full px-4 flex flex-wrap justify-center xl:mx-auto gap-1'>
           {
             projek.map((val)=>{
-               return <proyek key={val.id} judul={val.judul} deskripsi={val.deskripsi} url={val.url}/>
+               return <Proyek key={val.id} judul={val.judul} deskripsi={val.deskripsi} url={val.url}/>
             })
           }
         </div>
