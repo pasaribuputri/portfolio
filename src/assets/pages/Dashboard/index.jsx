d2ac42b823006312736e0b13710feb0af76b8e80
pasaribuputri <pasaribuutri02@gmail.com>
2023-07-26 16:08:20 +0700

    ..

--- src/assets/pages/Dashboard/index.jsx
+++ src/assets/pages/Dashboard/index.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
           </div>
           <div className="w-full self-end px-4 lg:w-1/2">
             <div className="mt-8 relative lg:mt-0 lg:right-0 ">
-              <img src="/poto.png" alt="Putri Mulyani" className='w-96 rounded-full ml-24'/>
+              <img src="/poto.png" alt="Putri Mulyani" className='w-96 rounded-full lg:ml-24'/>
               {/* <span className='absolute -bottom-0 -z-10 left-1/2 -translate-x-1/2 md:scale-125 '>
                 <svg height='400' width='400' viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                 <path  fill="#14b8a6" d="M56.2,-24.2C61.4,-2.2,46.3,20.4,27.5,32.8C8.8,45.2,-13.6,47.4,-33.6,35.5C-53.5,23.6,-70.9,-2.6,-64.9,-25.6C-58.8,-48.7,-29.4,-68.8,-2,-68.2C25.5,-67.5,51,-46.2,56.2,-24.2Z" transform='translate(100 100) scale(1.1)' />
